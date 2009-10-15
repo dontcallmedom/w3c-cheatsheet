c711b24e05b9a438cd9a9ba6906d58a6a4dbbe74
Dominique Hazaël-Massieux <dom@w3.org>
2009-10-15 09:55:50 +0000

    support for css selectors

--- js/start.js
+++ js/start.js
@@ -14,7 +14,8 @@ make_unique = function(b)
  }
 
 keywordSources = {
-	"CSS":[{"list":[],"details":cssPropertiesDetails,"name":"CSS Property"}], 
+	"CSS":[{"list":[],"details":cssPropertiesDetails,"name":"CSS Property"},
+	       {"list":[],"details":cssSelectorsDetails,"name":"CSS Selector"}], 
 	"HTML":[
 	{"list":[],"details":htmlElementsDetails,"name":"HTML Element"},
 	{"list":[],"details":htmlAttributesDetails,"name":"HTML attribute"}],
