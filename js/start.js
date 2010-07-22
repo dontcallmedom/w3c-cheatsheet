1f5b05826c5e343d409d5ec5dc153f2a40c2761b
Dominique Hazaël-Massieux <dom@w3.org>
2010-07-22 14:49:45 +0000

    removing currently unused cookie stuff

--- js/start.js
+++ js/start.js
@@ -284,35 +284,6 @@ Cheatsheet.prototype.load_anchor = function (anchor) {
     return false;
 };
 
-/*
- * Simple wrapper for setting cookies
- */
-Cheatsheet.prototype.setCookie = function (c_name, value, expiredays) {
-    var exdate = new Date();
-    exdate.setDate(exdate.getDate() + expiredays);
-    document.cookie = c_name + "=" + escape(value) +
-        ((expiredays === null) ? "" : ";expires=" + exdate.toGMTString());
-};
-
-/*
- * Simple wrapper for reading cookies
- */
-Cheatsheet.prototype.getCookie = function (c_name) {
-    if (document.cookie.length > 0) {
-        var c_start = document.cookie.indexOf(c_name + "=");
-        if (c_start !== -1) {
-            c_start = c_start + c_name.length + 1;
-            var c_end = document.cookie.indexOf(";", c_start);
-            if (c_end === -1) {
-                c_end = document.cookie.length;
-            }
-            return unescape(document.cookie.substring(c_start, c_end));
-        }
-    }
-    return "";
-};
-
-
 var cheatsheet = new Cheatsheet();
 
 /*
